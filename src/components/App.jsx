84c728ba4da31072987423402e331318ec384356
stckpntr <73133150+stckpntr@users.noreply.github.com>
2022-03-07 00:20:39 +0100

    working version of props practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,24 @@
 import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
 
 function App() {
+  const cardArray = [];
+  contacts.forEach((element) => {
+    cardArray.push(
+      <Card
+        name={element.name}
+        img={element.imgURL}
+        tel={element.phone}
+        email={element.email}
+      />
+    );
+  });
+
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      {cardArray}
     </div>
   );
 }
